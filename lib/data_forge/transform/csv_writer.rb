4d4490a6fb67b7723746c3087bd10ea399a606cd
Zoltan Ormandi <zoltan.ormandi@emarsys.com>
2013-11-22 16:58:28 +0100

    Refactoring

--- lib/data_forge/transform/csv_writer.rb
+++ lib/data_forge/transform/csv_writer.rb
@@ -3,7 +3,8 @@ module DataForge
     module CSVWriter
 
       def write_csv_file(file_descriptor)
-        csv_file = CSV.open "#{file_descriptor.name.to_s}.csv", "w:UTF-8", csv_write_options_for(file_descriptor)
+        csv_file = CSV.open "#{file_descriptor.name.to_s}.csv", "w:UTF-8", { write_headers: true,
+                                                                             headers: file_descriptor.field_names }
         begin
           yield csv_file
         ensure
@@ -15,15 +16,6 @@ module DataForge
 
       def output_record_to_file(record, fields, file)
         file << fields.map { |field| record[field] }
-      end
-
-
-
-      private
-
-      def csv_write_options_for(file_descriptor)
-        { write_headers: true,
-          headers: file_descriptor.field_names }
       end
 
     end
