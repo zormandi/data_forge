a632ce421d08aba9131e3f753a6d88c2b87cf4f6
Zoltan Ormandi <zoltan.ormandi@emarsys.com>
2014-06-27 17:42:41 +0200

    Refactoring

--- spec/data_forge_spec.rb
+++ spec/data_forge_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe DataForge do
 
   it "should have a version number" do
-    expect(DataForge::VERSION).to_not be_nil
+    expect(DataForge::VERSION).not_to be_nil
   end
 
 
