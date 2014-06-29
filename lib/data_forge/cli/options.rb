21423965b19dc5e3ef0e44f574b80ca818bd0c81
Zoltan Ormandi <zoltan.ormandi@emarsys.com>
2014-06-29 16:38:41 +0200

    Refactoring

--- lib/data_forge/cli/options.rb
+++ lib/data_forge/cli/options.rb
@@ -9,29 +9,29 @@ module DataForge
 
         options = new
 
-        OptionParser.new do |opts|
-          opts.default_argv = args
-          opts.banner = "Usage: [bundle exec] forge [options] command_script.rb"
+        OptionParser.new do |parser|
+          parser.default_argv = args
+          parser.banner = "Usage: [bundle exec] forge [options] command_script.rb"
 
-          opts.separator ""
-          opts.separator "Options:"
+          parser.separator ""
+          parser.separator "Options:"
 
-          opts.on("-Uname=value",
+          parser.on("-Uname=value",
                   /^(?<name>\w+)=(?<value>\S+)$/,
                   "User-defined parameter value to be passed to the command script.",
                   "Can be specified multiple times (with a different name).") do |_, name, value|
             options.user_params[name.to_sym] = value
           end
 
-          opts.separator ""
-          opts.separator "Common options:"
+          parser.separator ""
+          parser.separator "Common options:"
 
-          opts.on_tail("-h", "--help", "Show this message") do
-            output.puts opts
+          parser.on_tail("-h", "--help", "Show this message") do
+            output.puts parser
             options.execute = false
           end
 
-          opts.on_tail("-v", "--version", "Show version information") do
+          parser.on_tail("-v", "--version", "Show version information") do
             output.puts "DataForge, version #{DataForge::VERSION}"
             options.execute = false
           end
