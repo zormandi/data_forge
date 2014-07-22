aa65cf489d49fe194e3a7248309def527e1f0422
Zoltan Ormandi <zoltan.ormandi@emarsys.com>
2014-07-22 20:23:26 +0200

    Refactoring

--- lib/data_forge/file/record_file_definition.rb
+++ lib/data_forge/file/record_file_definition.rb
@@ -2,27 +2,30 @@ module DataForge
   module File
     module RecordFileDefinition
 
-      def self.from_input(name, &initializer_block)
-        create name, &initializer_block
-      end
+      class << self
 
-
-
-      def self.from_existing(definition_to_copy, name, &customization_block)
-        create name, definition_to_copy, &customization_block
-      end
-
-
-
-      private
-
-      def self.create(name, definition_to_copy = nil, &initializer_block)
-        CSV::CSVRecordFileDefinition.new(name).tap do |definition|
-          definition.copy definition_to_copy if definition_to_copy
-          definition.instance_eval &initializer_block if initializer_block
+        def from_input(name, &initializer_block)
+          create name, &initializer_block
         end
-      end
 
+
+
+        def from_existing(definition_to_copy, name, &customization_block)
+          create name, definition_to_copy, &customization_block
+        end
+
+
+
+        private
+
+        def create(name, definition_to_copy = nil, &initializer_block)
+          CSV::CSVRecordFileDefinition.new(name).tap do |definition|
+            definition.copy definition_to_copy if definition_to_copy
+            definition.instance_eval &initializer_block if initializer_block
+          end
+        end
+
+      end
 
 
       public
